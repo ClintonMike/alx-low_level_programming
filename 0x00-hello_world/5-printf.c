76dc43721d8809437954003b8e50cad735daac06
ClintonMike <124153049+ClintonMike@users.noreply.github.com>
2023-02-16 17:37:56 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main -entry point of the program.
+ * main -entry point of program
  *
- * Return:zero if no error, Non-zero value  if errors
+ * Return: zero if no erros, non-zero if error
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
